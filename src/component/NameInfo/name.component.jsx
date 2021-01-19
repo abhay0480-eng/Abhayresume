e9e4240b9d7c79ddd4ef51881ef4b80cf4b62b65
Abhay kumar <abhay0480sharma@gmail.com>
2021-01-19 16:32:03 +0530

    add name info and contact info

---
+++ src/component/NameInfo/name.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Name(){
+    return(
+        <div className="heading">
+        <h1>Abhay Kumar</h1>
+        </div>
+    )
+}
+
+export default Name;
---
+++ src/component/contanctinfo/contact.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact(){
+    return(
+     <div className="contact">
+     <p>+91 7073271758 | abhay0480sharma@gmail.com </p>
+     <p>Linkedin : <a href="https://www.linkedin.com/in/abhay-kumar-587617155/">Profile</a></p>
+     </div>
+    );
+}
+
+export default Contact;
